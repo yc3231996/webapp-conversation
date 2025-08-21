bb3887e67130ae77c62621d35d8576021cc1d51f
Yancy <yc3231996@gmail.com>
2025-08-21 17:35:29 +0800

    fix i18n和登陆问题

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,6 +2,15 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export async function POST() {
-  cookies().delete('session')
+  // When deleting a cookie, you must pass the same options (especially `secure` and `path`)
+  // that were used to set the cookie.
+  cookies().set({
+    name: 'session',
+    value: '',
+    path: '/',
+    maxAge: -1, // Expire the cookie immediately
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  })
   return NextResponse.json({ message: 'Logout successful' })
 }
